e9b5d73dff7902b437960f120af6ebd61915043f
subhava06 <subhav.22gcebcs190@galgotiacollege.edu>
2025-03-02 00:07:05 +0530

    Learning Kotlin

---
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -0,0 +1,13 @@
+package com.example.firstkotlinapp
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+
+
+fun main()
+{
+    println("Hello World!");
+}
