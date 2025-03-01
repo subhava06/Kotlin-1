cbbb7b149f7ad1ebbe4233d2c9258da5fb6d914b
subhava06 <subhav.22gcebcs190@galgotiacollege.edu>
2025-03-02 01:12:40 +0530

    Variables and Datatypes in Kotlin

--- app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
+++ app/src/main/java/com/example/firstkotlinapp/MainActivity.kt
@@ -9,5 +9,31 @@ import androidx.core.view.WindowInsetsCompat
 
 fun main()
 {
-    println("Hello World!");
+    //declaring a variable
+    var userName: String = "Subhava Ojha"
+    userName = "Alex"
+
+    //declaring a variable
+    var age = 22 // due to type inference
+
+    println("Hello $userName! age is $age")
+
+    var maxIntegerValue: Int = Int.MAX_VALUE
+    var minIntegerValue = Int.MIN_VALUE
+    println("Maximum and minimum value of Int is : $maxIntegerValue and $minIntegerValue")
+
+    var maxByteValue: Byte = Byte.MAX_VALUE
+    var minByteValue: Byte = Byte.MIN_VALUE
+    println("Maximum and minimum value of Byte is : $maxByteValue and $minByteValue")
+
+
+    var maxShortValue: Short = Short.MAX_VALUE
+    var minShortValue: Short = Short.MIN_VALUE
+    println("Maximum and minimum value of Short is : $maxShortValue and $minShortValue")
+
+    var maxLongValue: Long = Long.MAX_VALUE
+    var minLongValue: Long = Long.MIN_VALUE
+    println("Maximum and minimum vlaue of Long is : $maxLongValue and $minLongValue")
+
+
 }
